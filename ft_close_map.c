202cc8189ec574ee257f0c7d92072bd8abe3058b
Kosuke Kawano <kkawano@student.42tokyo.jp>
2021-10-31 23:52:34 +0900

    a

--- ft_close_map.c
+++ ft_close_map.c
@@ -6,7 +6,7 @@
 /*   By: kkawano <kkawano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 16:46:43 by kkawano           #+#    #+#             */
-/*   Updated: 2021/10/26 12:30:14 by kkawano          ###   ########.fr       */
+/*   Updated: 2021/10/31 23:51:50 by kkawano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	ft_close_map(int keycode, t_data *data)
 {
-    (void)keycode;
-    (void)data;
-    exit(EXIT_SUCCESS);
-    return (0);
+	(void)keycode;
+	(void)data;
+	exit(EXIT_SUCCESS);
+	return (0);
 }
